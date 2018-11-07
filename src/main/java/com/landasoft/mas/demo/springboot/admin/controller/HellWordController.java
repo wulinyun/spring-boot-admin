07d56a7116e90369691338585018e27ae31c6896
wulinyun <lin.wu@landasoft.com>
2018-11-07 19:55:13 +0800

    更新

--- src/main/java/com/landasoft/mas/demo/springboot/admin/controller/HellWordController.java
+++ src/main/java/com/landasoft/mas/demo/springboot/admin/controller/HellWordController.java
@@ -8,10 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HellWordController {
 	@Autowired
 	private Environment environment;
-	@RequestMapping("/")
-    public String home() {
-        return "Spring Boot Admin Hello World ！！！！！！";
-    }
 	@RequestMapping("/getEnv")
     public String getEnv(String env) {
         return environment.getProperty(env);
