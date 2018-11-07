f1b9a6b051bd0701e0a5e7530fc046d6caf67f85
wulinyun <lin.wu@landasoft.com>
2018-11-07 18:07:01 +0800

    开启注册中心

--- src/main/java/com/landasoft/mas/demo/springboot/admin/SpringBootAdminApplication.java
+++ src/main/java/com/landasoft/mas/demo/springboot/admin/SpringBootAdminApplication.java
@@ -2,6 +2,7 @@ package com.landasoft.mas.demo.springboot.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 /*import org.springframework.cloud.netflix.eureka.EnableEurekaClient;*/
 import org.springframework.context.annotation.Configuration;
 
@@ -10,7 +11,7 @@ import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
 @SpringBootApplication
 @EnableAdminServer
-/*@EnableEurekaClient*/
+@EnableEurekaClient
 public class SpringBootAdminApplication {
 
 	public static void main(String[] args) {
