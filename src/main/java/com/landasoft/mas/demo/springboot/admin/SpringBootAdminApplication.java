6236b1801ad9b3c1279ac6e4485b88e4a2800887
wulinyun <lin.wu@landasoft.com>
2018-11-07 18:53:32 +0800

    spring-boot-admin

--- src/main/java/com/landasoft/mas/demo/springboot/admin/SpringBootAdminApplication.java
+++ src/main/java/com/landasoft/mas/demo/springboot/admin/SpringBootAdminApplication.java
@@ -4,9 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 /*import org.springframework.cloud.netflix.eureka.EnableEurekaClient;*/
-import org.springframework.context.annotation.Configuration;
-
-import de.codecentric.boot.admin.server.config.AdminServerProperties;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
 @SpringBootApplication
