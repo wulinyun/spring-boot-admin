4f4fc2fe55e95d562d8e9cb8dca96fac43eb0e84
wulinyun <lin.wu@landasoft.com>
2018-11-07 19:51:53 +0800

    更新

--- src/main/java/com/landasoft/mas/demo/springboot/admin/controller/HellWordController.java
+++ src/main/java/com/landasoft/mas/demo/springboot/admin/controller/HellWordController.java
@@ -1,11 +1,19 @@
 package com.landasoft.mas.demo.springboot.admin.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HellWordController {
+	@Autowired
+	private Environment environment;
 	@RequestMapping("/")
     public String home() {
         return "Spring Boot Admin Hello World ！！！！！！";
+    }
+	@RequestMapping("/getEnv")
+    public String getEnv(String env) {
+        return environment.getProperty(env);
     }
 }
